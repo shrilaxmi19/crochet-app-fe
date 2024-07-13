ec4dc5844438427121784af68026237b84a11677
Shrilaxmi <shrilaxmi.n@spurtreetech.com>
2024-07-13 11:33:48 +0530

    New

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -72,7 +72,7 @@ const SearchPage = () => {
   }
 
   if (!results?.data || !city) {
-    return <span>No results found. Please enter a valid City</span>;
+    return <span>Loading...</span>;
   }
 
   return (
